41b4b572563a1528acba527f503aecfb3a70a00a
clangton2@dxc.com <clangton2@dxc.com>
2020-02-17 15:20:06 +0000

    Add the viewer
    Add a larger viewer

--- polaroid.js
+++ polaroid.js
@@ -3,7 +3,8 @@
 function makePol(x) {
   for ( var i = 1 ; i <= x ; i++ ) {
     img = eval("img_"+i);			// use the counter to evaluate the input
-    html = "<div class='polaroid " + img[4] +"'>";
+    html = "<a href='./viewer.html?"+ img[0] + ";" + img[1] + ";" + img[2] + ";" + img[3] + "'>";
+    html += "<div class='polaroid " + img[4] +"'>";
     html += "<img class='polaroid' src='" + img[0] + "' style = width:100%>";
     if (img[2] != '') {
       html += "<div class='price'> &pound;" + img[2] + "</div>";
@@ -15,7 +16,7 @@ function makePol(x) {
     } else {
       html += "<div class='title'>" + img[3] + "<br><div class='media'>  " + img[1] + "</div></div>";
     }
-    html += "</div>"
+    html += "</div></a>"
     document.write(html);
 }
 }
