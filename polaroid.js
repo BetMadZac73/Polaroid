32ba11bd2c12c999868394a1899559ccb255c486
clangton2@dxc.com <clangton2@dxc.com>
2020-02-23 09:16:07 +0000

    Adding Content and Styling
    Added a bunch of content (pictures), added in the option to add latest work as a spot light (bigger polaroid).
    
    Also updated the hover styling for the polaroid so that we get increase in size x1.1 and polaroid background turns from white to black.

--- polaroid.js
+++ polaroid.js
@@ -1,11 +1,19 @@
 // functions
 
 function makePol(x) {
-  for ( var i = 1 ; i <= x ; i++ ) {
+//  for ( var i = 1 ; i <= x ; i++ ) {
+  for ( var i = x ; i >= 0 ; i-- ) {            // lets run in reverse
     img = eval("img_"+i);			// use the counter to evaluate the input
+    if (img[5] == 'big') {
+      big = "style='width:600px; height:600px; margin:auto;'"
+      bigpic = "style='height:500px;'"
+    } else {
+      big = "";
+      bigpic = "";
+    }
     html = "<a href='./viewer.html?"+ img[0] + ";" + img[1] + ";" + img[2] + ";" + img[3] + "'>";
-    html += "<div class='polaroid " + img[4] +"'>";
-    html += "<img class='polaroid' src='" + img[0] + "' style = width:100%>";
+    html += "<div class='polaroid " + img[4] +"' " +big+ ">";
+    html += "<img class='polaroid' src='" + img[0] + "' "+bigpic+" >";
     if (img[2] != '') {
       html += "<div class='price'> &pound;" + img[2] + "</div>";
     } else {
